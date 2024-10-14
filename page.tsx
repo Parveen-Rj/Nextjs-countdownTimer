e04d3709badd879210f2465fcf297350810904b8
Parveen-Rj <165608618+Parveen-Rj@users.noreply.github.com>
2024-10-14 11:54:35 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,11 @@
+// app/page.tsx
+import CountdownTimer from './components/CountdownTimer'
+
+export default function Home() {
+  return (
+    <div>
+      <CountdownTimer />
+      
+    </div>
+  );
+}
